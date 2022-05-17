d5463e939f695a3eee2b70d317fe1208cd7d6e2c
jujang <jeongookjang@naver.com>
2022-05-17 21:51:58 +0900

    Nomad Challenge#7 for VanillaJS

--- Nomad_Coder_Challenge/Assignment7/src/index.js
+++ Nomad_Coder_Challenge/Assignment7/src/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const body = document.querySelector("body");
 
 const h1 = document.createElement('h1');
@@ -9,6 +11,78 @@ const h3Span1 = document.createElement('span');
 h3Span1.innerText = 'Generate a number between 0 and ';
 h3.appendChild(h3Span1);
 const inputNumberRange = document.createElement('input');
-let numberRange = inputNumberRange.value;
+let numberRange;
 h3.appendChild(inputNumberRange);
 body.appendChild(h3);
+
+const guess = document.createElement('div');
+const guessSpan = document.createElement('span');
+guessSpan.innerText = 'Guess the number: ';
+const guessInput = document.createElement('input');
+let guessingNum;
+const guessButton = document.createElement('button');
+guessButton.innerText = 'play!';
+guess.appendChild(guessSpan);
+guess.appendChild(guessInput);
+guess.appendChild(guessButton);
+body.appendChild(guess);
+
+const state = document.createElement('div');
+const stateSpan1 = document.createElement('span');
+const stateSpan2 = document.createElement('span');
+state.appendChild(stateSpan1);
+state.appendChild(stateSpan2);
+body.appendChild(state);
+
+const b = document.createElement('b');
+body.appendChild(b);
+
+
+
+function inputCheck(numberRange, guessingNum){
+    if(numberRange != null && numberRange >= 0){
+        if(guessingNum != null && guessingNum >= 0 && guessingNum <= numberRange) {
+            return 1;
+        } else {
+            alert('Please enter a number greater than or equal to 0 and less than or equal to ' + numberRange + ' to the second blank');
+            return 0;
+        }
+    } else {
+        alert('Please write 0 or greater than 0 to the first blank');
+        return 0;
+    }
+}
+
+function createRandomNum(numberRange){
+    let randomNum = Math.floor(Math.random()*(numberRange+1));
+    return randomNum;
+}
+function doGame(randomNum){
+    let result;
+    if(randomNum == guessingNum) {
+        result = 'You won!';
+        return result;
+    } else {
+        result = 'You lost!';
+        return result;
+    }
+}
+
+function inputText(guessingNum, randomNum, result) {
+    stateSpan1.innerText = 'You chose: ' + guessingNum;
+    stateSpan2.innerText = ', the machine chose: ' + randomNum + '.';
+    b.innerText = result;
+}
+
+
+function printResult(){
+    numberRange = parseInt(inputNumberRange.value, 10);
+    guessingNum = parseInt(guessInput.value, 10);
+    if(inputCheck(numberRange, guessingNum)) {
+        let randomNum = createRandomNum(numberRange);
+        let result = doGame(randomNum);
+        inputText(guessingNum, randomNum, result);
+    };
+}
+
+guessButton.addEventListener('click', printResult);
