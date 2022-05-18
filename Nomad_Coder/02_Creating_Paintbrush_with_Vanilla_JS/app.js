d533454f458763f176d5abebd3f01843a6c8e979
jujang <jeongookjang@naver.com>
2022-05-19 01:47:41 +0900

    Study about creating paintbrush by vanillaJS from Nomad_Coder

---
+++ Nomad_Coder/02_Creating_Paintbrush_with_Vanilla_JS/app.js
@@ -0,0 +1,88 @@
+const canvas = document.querySelector('#jsCanvas');
+const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll('.jsColor');
+const range = document.querySelector('#jsRange');
+const mode = document.querySelector('#jsMode');
+
+const INITIAL_COLOR = '#2c2c2c';
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
+
+
+let painting = false;
+let filling = false;
+
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.moveTo(x, y);
+        ctx.beginPath();
+    } else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeChange(event){
+    if(filling == true) {
+        filling = false;
+        mode.innerText = 'Fill';
+    } else {
+        filling = true;
+        mode.innerText = 'Paint';
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("mousedown", handleCanvasClick);
+}
+
+Array.from(colors).forEach(color => {
+    color.addEventListener("click", handleColorClick);
+});
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeChange);
+}
