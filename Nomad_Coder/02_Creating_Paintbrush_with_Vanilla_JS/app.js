03f42ad6cd1c9da32184b06a636069d1d6cfcb25
jujang <jeongookjang@naver.com>
2022-05-19 03:16:41 +0900

    Study about creating paintbrush by vanillaJS from Nomad_Coder

--- Nomad_Coder/02_Creating_Paintbrush_with_Vanilla_JS/app.js
+++ Nomad_Coder/02_Creating_Paintbrush_with_Vanilla_JS/app.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll('.jsColor');
 const range = document.querySelector('#jsRange');
 const mode = document.querySelector('#jsMode');
+const saveBtn = document.querySelector('#jsSave');
+
 
 const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 700;
@@ -10,6 +12,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -67,12 +71,26 @@ function handleCanvasClick(){
     }
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = 'PaintJS[🎨]';
+    link.click();
+}
+
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("mousedown", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => {
@@ -85,4 +103,8 @@ if(range){
 
 if(mode) {
     mode.addEventListener("click", handleModeChange);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
